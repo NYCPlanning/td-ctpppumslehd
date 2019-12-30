2a8985cc19c1879df7aa9f9906570ca507eb5256
mayijun1203 <mayijun1203@gmail.com>
2019-12-30 12:06:24 -0500

    Update ctpp.R

--- ctpp.R
+++ ctpp.R
@@ -1,6 +1,7 @@
 library(dplyr)
 
 path='C:/Users/Y_Ma2/Desktop/CTPPPUMSLEHD/'
+quadstate=c('CT','NJ','NY','PA')
 
 df=read.csv(paste0(path,'CTPP/FLOW/NY_2012thru2016_A302103.csv'),stringsAsFactors = FALSE,colClasses = 'character')
 df=filter(df,substr(df$GEOID,1,5)=='C5400')
