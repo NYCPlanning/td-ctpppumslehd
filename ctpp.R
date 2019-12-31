be6df527c2051f4c9dfb779437b16d01704386e6
mayijun1203 <mayijun1203@gmail.com>
2019-12-31 13:50:27 -0500

    Update ctpp.R

--- ctpp.R
+++ ctpp.R
@@ -1,13 +1,17 @@
 library(dplyr)
 
-path='C:/Users/Y_Ma2/Desktop/CTPPPUMSLEHD/'
+# path='C:/Users/Y_Ma2/Desktop/CTPPPUMSLEHD/'
+path='I:/CTPPPUMSLEHD/'
 quadstate=c('CT','NJ','NY','PA')
 
-df=read.csv(paste0(path,'CTPP/FLOW/NY_2012thru2016_A302103.csv'),stringsAsFactors = FALSE,colClasses = 'character')
+df=data.frame()
+for (i in quadstate){
+  df=rbind(df,read.csv(paste0(path,paste0('CTPP/FLOW/',as.character(i),'_2012thru2016_A302103.csv')),stringsAsFactors = FALSE,colClasses = 'character'))
+}
 df=filter(df,substr(df$GEOID,1,5)=='C5400')
 df$RESCT=substr(df$GEOID,8,18)
 df$WORKCT=substr(df$GEOID,19,29)
 df$MODE=as.integer(df$LINENO)
 df$EST=as.numeric(gsub(',','',df$EST))
-df$MOE=as.numeric(gsub('+/-','',df$MOE))
+df$MOE=as.numeric(gsub(',','',gsub('+/-','',df$MOE)))
 df=select(df,RESCT,WORKCT,MODE,EST,MOE)
